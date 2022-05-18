38d18f6b3b5bc683a6d438b2597791ab84807cb8
hiep7602 <97502868+hiep7602@users.noreply.github.com>
2022-05-18 23:02:05 +0700

    Add files via upload

---
+++ BaiThucHanh1705/src/App88.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+public class App88 {
+    public static void main (String[]args){
+        HashMap<String, String> hashMapCity = new HashMap<>();
+        hashMapCity.put("HN", "Hà Nội");
+        hashMapCity.put("HD", "Hải Dương");
+        hashMapCity.put("Th", "Thanh Hóa");
+        hashMapCity.put("NA", "Nghệ An");
+
+        System.out.println("Danh sách các thành phố trong hashMapCity:");
+        Set<Map.Entry<String, String>> setCity = hashMapCity.entrySet();
+        System.out.println(setCity);
+
+        System.out.println("HN: " + hashMapCity.get ("HN"));
+        System.out.println("BN:" + hashMapCity.get ("BN"));
+
+        if (hashMapCity.containsValue("Thành phố Hà Nội"));
+            System.out.println("Có thành phố Hà Nội trong hashMapCity");
+    }
+}
